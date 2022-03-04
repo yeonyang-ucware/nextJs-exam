98060d7c8e7f1b1ae8774db8db1a396c57a2f647
yeonyang-ucware <100828805+yeonyang-ucware@users.noreply.github.com>
2022-03-04 16:22:37 +0900

    first commit

---
+++ pages/api/about.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title> about me</title>
+            </Head>
+            <h1> Aboout</h1>
+        </div>
+    )
+}
+
+export default about
